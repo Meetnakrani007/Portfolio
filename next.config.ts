e326924b7e2c048c28e797bcb9bdc0746d0a93d1
Meet Nakrani <nakranimeet2005@gmail.com>
2025-07-09 12:14:47 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,10 @@ const nextConfig: NextConfig = {
     ],
   },
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
